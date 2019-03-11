b3a8a63879ed3cfb23837c630823b844cfd0188a
raveenraj666 <48232360+raveenraj666@users.noreply.github.com>
2019-03-11 10:28:36 +0800

    Update HelloWorld.php

--- vendor/Project/HelloWorld.php
+++ vendor/Project/HelloWorld.php
@@ -6,12 +6,10 @@ namespace Vendor\Project;
 class HelloWorld
 {
     public function getHiAtoum ()
-    {
-        <button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
+    
+  {
+        return 'Hi atou !';
+    
 
     }
 }
